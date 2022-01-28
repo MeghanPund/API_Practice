7bd97c613793533d07e7525d80fd09dfa81bebd6
Meghan Pund <meghan.pund@gmail.com>
2022-01-27 20:25:18 -0500

    made the units imperial

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import requests
 import api_key
 
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Louisville,kentucky&appid={api_key.key}")
+response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Louisville,kentucky&appid={api_key.key}&units=imperial")
 request = response.request
 
+
 f = open("weather.json", 'a')
-f.write(response.text)
+f.write(f"{response.text}\n")
+
