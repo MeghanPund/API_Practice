63764e1b7b0aa475b84da97e7a381a728872a4ac
Meghan Pund <meghan.pund@gmail.com>
2022-01-28 10:13:45 -0500

    playing with calling API!!!

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import json
 import requests
 import api_key
 
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Louisville,kentucky&appid={api_key.key}&units=imperial")
+url = f"https://api.openweathermap.org/data/2.5/weather?q=Louisville,kentucky&appid={api_key.key}&units=imperial"
+response = requests.get(url)
 content_type = response.headers.get('Content-Type')
 request = response.request
 
+json_data = response.json()
 
-f = open("weather.json", 'a')
-f.write(f"{response.json()}\n")
+tempF = json_data['main']['temp']
+turbulence = json_data['wind']['speed']
+rain_or_shine = json_data['clouds']['all']
+print(tempF)
+print(turbulence)
+print(rain_or_shine)
+
+# f = open("weather.json", 'a')
+# f.write(f"{response.json()}\n")
 
