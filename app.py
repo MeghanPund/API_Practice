59bf98daefca3915b974a5c19a38ccec4a310dcc
Meghan Pund <meghan.pund@gmail.com>
2022-01-27 20:42:26 -0500

    content type and .json()

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import requests
 import api_key
 
 response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=Louisville,kentucky&appid={api_key.key}&units=imperial")
+content_type = response.headers.get('Content-Type')
 request = response.request
 
 
 f = open("weather.json", 'a')
-f.write(f"{response.text}\n")
+f.write(f"{response.json()}\n")
 
