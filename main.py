b1d5ec73f7d6a8fbc1a6a1038e05f0f44541d75e
Meghan Pund <meghan.pund@gmail.com>
2022-02-11 17:05:59 -0500

    flake8 for pep8

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def index():
                 location = (f'{city.strip()},{state.strip()}')
             else:
                 location = city.strip()
-            
+
             index.url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key.key}'
-            
+
             if units == 'metric':
                 index.url += metric
             else:
@@ -59,20 +59,21 @@ def analyze_weather():
     humidity = json_data['main']['humidity']
 
     quotes = ['“I don\'t run to add days to my life, I run to add life to my days.” — Ronald Rook',
-    '“Running is the greatest metaphor for life, because you get out of it what you put into it.” — Oprah Winfrey',
-    '“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” — Will Durant on Aristotle',
-    '“If it doesn\'t challenge you, it won\'t change you.” — Fred DeVito',
-    '“The man who moves a mountain begins by carrying away small stones.” — Confucius',
-    '“Someone who is busier than you is running right now.” — Nike',
-    '"Only those who risk going too far can possibly find out how far one can go." — T. S. Eliot.',
-    '“The will to win means nothing without the will to prepare.” — Juma Inkangaa, 1989 winner of the New York Marathon',
-    '“Fill the unforgiving minute with sixty seconds worth of distance run.” — Rudyard Kipling',
-    '“Forever is composed of nows.” — Emily Dickinson',
-    '“The miracle isn\'t that I finished. The miracle is that I had the courage to start.” ― John Bingham',
-    ]
+              '“Running is the greatest metaphor for life, because you get out of it what you put into it.” — Oprah Winfrey',
+              '“We are what we repeatedly do. Excellence, then, is not an act, but a habit.” — Will Durant on Aristotle',
+              '“If it doesn\'t challenge you, it won\'t change you.” — Fred DeVito',
+              '“The man who moves a mountain begins by carrying away small stones.” — Confucius',
+              '“Someone who is busier than you is running right now.” — Nike',
+              '"Only those who risk going too far can possibly find out how far one can go." — T. S. Eliot.',
+              '“The will to win means nothing without the will to prepare.” — Juma Inkangaa, 1989 winner of the New York Marathon',
+              '“Fill the unforgiving minute with sixty seconds worth of distance run.” — Rudyard Kipling',
+              '“Forever is composed of nows.” — Emily Dickinson',
+              '“The miracle isn\'t that I finished. The miracle is that I had the courage to start.” ― John Bingham',
+              ]
     quote = random.choice(quotes)
-    
+
     return render_template('attireweather.html', quote=quote, location=location, units=units, temp=temp, tempF=tempF, temp_feel=temp_feel, rain_chance=rain_chance, precip=precip, turbulence=turbulence, humidity=humidity)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
